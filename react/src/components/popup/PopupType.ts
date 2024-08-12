fa8fe4b3eb07194a6209e10cdad528442c0a6203
sonseongyeol <sonseong10@gmail.com>
2024-08-12 20:17:19 +0900

    불필요한 타입제거

--- react/src/components/popup/PopupType.ts
+++ react/src/components/popup/PopupType.ts
@@ -3,25 +3,21 @@ import { lazy } from "react";
 
 // NOTE: 팝업타입
 export enum PopupType {
-  // 어드민사이트 및 통합AD Popup
-  ALERT = "alert",
-  INPUT = "input",
-  CONFIRM = "confirm",
-  WARNING = "warning",
-  IMAGE = "image",
-  GRID = "Grid",
-  ICON = "Icon",
-  SCRIPT = "Script",
+  ALERT = 'alert',
+  INPUT = 'input',
+  CONFIRM = 'confirm',
+  WARNING = 'warning',
+  IMAGE = 'image',
+  GRID = 'Grid',
+  ICON = 'Icon',
 }
 
 // 어드민사이트 및 전역통합AD Popup
 const Image = lazy(() => import("./components/ImagePopup"));
 const Grid = lazy(() => import("./components/GridPopup"));
-const ICon = lazy(() => import("./components/IconPopup"));
-const Script = lazy(() => import("./components/ScriptPopup"));
+const ICon = lazy(() => import('./components/IconPopup'));
 
 // 어드민사이트 및 통합AD Popup
 addPopup(PopupType.IMAGE, Image);
 addPopup(PopupType.GRID, Grid);
 addPopup(PopupType.ICON, ICon);
-addPopup(PopupType.SCRIPT, Script);
